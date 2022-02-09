02e220bbd76fdd6f1844093da855c3d9507572ae
niyazidageek <niyazinb@code.edu.az>
2022-02-09 12:38:33 +0400

    Answer filter by user and question added

--- api/v1/answer/urls.py
+++ api/v1/answer/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns=[
     path('', views.AnswerAPI.as_view()),
-    # path('<int:pk>', views.QuestionRetrieveUpdateDestroyAPI.as_view()),
+    path('<int:pk>', views.AnswerRetrieveDestroyAPI.as_view()),
 ]
--- api/v1/answer/views.py
+++ api/v1/answer/views.py
@@ -4,6 +4,7 @@ from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from taskool.models import Option, File, Answer, TextAnswer, AudioAnswer
 from rest_framework.permissions import AllowAny
+from django_filters.rest_framework import DjangoFilterBackend
 from . import serializer
 
 
@@ -36,41 +37,11 @@ class AnswerAPI(ListCreateAPIView):
 
 class AnswerRetrieveDestroyAPI(RetrieveDestroyAPIView):
     serializer_class = serializer.AnswerSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['user','question']
 
     def get_queryset(self):
-        return Option.objects.filter(id=self.kwargs.get('pk', None))
-
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-
-        if serializer.validated_data.get('deleteable_files'):
-            for fileId in serializer.validated_data.get('deleteable_files'):
-                if any(fileId == efi.id for efi in instance.file_content.all()):
-                    file = next((i for i in instance.file_content.all() if i.id == fileId))
-                    file.media.delete()
-                    file.delete()
-
-        files = request.FILES.getlist('file_content')
-
-        if files:
-            request.data.pop('file_content')
-            option_qs = Option.objects.get(id=instance.id)
-            uploaded_files = []
-            for file in files:
-                content = File.objects.create(media=file, extension=file.__getattribute__('content_type'))
-                uploaded_files.append(content)
-
-            option_qs.file_content.add(*uploaded_files)
-
-        self.perform_update(serializer)
-
-        if getattr(instance, '_prefetched_objects_cache', None):
-            instance._prefetched_objects_cache = {}
-
-        return Response({"status": True, "message": "Option updated!", "data": serializer.data})
+        return Answer.objects.filter(id=self.kwargs.get('pk', None))
 
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
