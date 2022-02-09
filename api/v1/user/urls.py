7ae0808707dd2ba32a18afe978a98bff927b5f70
niyazidageek <niyazinb@code.edu.az>
2022-02-09 17:33:05 +0400

    Groups added

--- api/v1/user/urls.py
+++ api/v1/user/urls.py
@@ -3,11 +3,12 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView, TokenVerifyView,
 )
-from api.v1.user.views import RegisterAPI
+from api.v1.user.views import RegisterStudentAPI, RegisterTeacherAPI
 
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view()),
     path('refresh-token/', TokenRefreshView.as_view()),
     path('token-verify/', TokenVerifyView.as_view()),
-    path('register/', RegisterAPI.as_view()),
+    path('register-student/', RegisterStudentAPI.as_view()),
+    path('register-teacher/', RegisterTeacherAPI.as_view()),
 ]
--- api/v1/user/views.py
+++ api/v1/user/views.py
@@ -4,15 +4,32 @@ from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from api.v1.user.serializer import RegisterSerializer, UserSerializer
+from django.contrib.auth.models import Group
 
 
-class RegisterAPI(CreateAPIView):
+class RegisterStudentAPI(CreateAPIView):
     serializer_class = RegisterSerializer
     permission_classes = (AllowAny,)
 
     def create(self, request, *args, **kwargs):
+        student_group = Group.objects.get(name='Teacher')
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
+        student_group.user_set(serializer.data)
+        headers = self.get_success_headers(serializer.data)
+        return Response({"data":serializer.data}, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class RegisterTeacherAPI(CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = (AllowAny,)
+
+    def create(self, request, *args, **kwargs):
+        teacher_group = Group.objects.get(name='Student')
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        teacher_group.user_set(serializer.data)
         headers = self.get_success_headers(serializer.data)
         return Response({"data":serializer.data}, status=status.HTTP_201_CREATED, headers=headers)
