439b3415c233326a69b8ba128eaf0d7219475582
niyazidageek <niyazinb@code.edu.az>
2022-02-10 18:11:58 +0400

    CRUD operations for Quiz added

--- api/v1/quiz/urls.py
+++ api/v1/quiz/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns=[
     path('', views.QuizAPI.as_view()),
-    # path('<int:pk>', views.AnswerRetrieveDestroyAPI.as_view()),
+    path('<int:pk>', views.QuizRetrieveUpdateDestroyAPI.as_view()),
 ]
--- api/v1/quiz/views.py
+++ api/v1/quiz/views.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveDestroyAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import AllowAny
 from . import serializer
 from taskool.models import Quiz
@@ -20,3 +20,22 @@ class QuizAPI(ListCreateAPIView):
         return Response({"status": True,
                          "message": "Quiz added",
                          "data": serializer.data}, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class QuizRetrieveUpdateDestroyAPI(RetrieveUpdateDestroyAPIView):
+        serializer_class = serializer.QuizSerializer
+
+        def get_queryset(self):
+            return Quiz.objects.filter(id=self.kwargs.get('pk', None))
+
+        def update(self, request, *args, **kwargs):
+            partial = kwargs.pop('partial', False)
+            instance = self.get_object()
+            serializer = self.get_serializer(instance, data=request.data, partial=partial)
+            serializer.is_valid(raise_exception=True)
+            self.perform_update(serializer)
+
+            if getattr(instance, '_prefetched_objects_cache', None):
+                instance._prefetched_objects_cache = {}
+
+            return Response({"status": True, "message": "Quiz updated!", "data": serializer.data})
